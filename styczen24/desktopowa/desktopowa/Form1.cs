6d13da9196069f22ee6c46fbbe07fca5eda4e64f
plebz0 <148546907+plebz0@users.noreply.github.com>
2026-05-12 14:42:43 +0200

    styczen24

---
+++ styczen24/desktopowa/desktopowa/Form1.cs
@@ -0,0 +1,104 @@
+﻿using desktopowa.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktopowa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void numeberTextBox_Leave(object sender, EventArgs e)
+        {
+            if(numberTextBox.Text == "444")
+            {
+                photoPictureBox.Image = null;
+                fingerprintPictureBox.Image = null;
+            }
+            else
+            {
+                //Nie mam pojęcia jak inaczej to zrobić
+                if(numberTextBox.Text == "000")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._000_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._000_odcisk;
+                }
+                else if (numberTextBox.Text == "111")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._111_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._111_odcisk;
+                }
+                else if (numberTextBox.Text == "222")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._222_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._222_odcisk;
+                }
+                else if (numberTextBox.Text == "333")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._333_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._333_odcisk;
+                }
+                else if (numberTextBox.Text == "555")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._555_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._555_odcisk_;
+                }
+                else if (numberTextBox.Text == "666")
+                {
+                    photoPictureBox.Image = desktopowa.Properties.Resources._666_zdjecie;
+                    fingerprintPictureBox.Image = desktopowa.Properties.Resources._666_odcisk;
+                }
+            }
+            
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if(nameTextBox.Text == "" || lastNameTextBox.Text == "")
+            {
+                MessageBox.Show("Wprwoadź dane");
+
+            }
+            else
+            {
+                string color = "";
+                if (blueEyesButton.Checked) 
+                {
+                    color = "niebieski";
+                }
+                else if (greenEyesButton.Checked)
+                {
+                    color = "zielone";
+                }
+                else if (hazelEyesButton.Checked)
+                {
+                    color = "piwne";
+                }
+                MessageBox.Show(nameTextBox.Text + " " + lastNameTextBox.Text + " kolor oczu " + color);
+            }
+        }
+    }
+}
---
+++ styczen24/konsola/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static char checkGender(string pesel)
+        {
+            return (pesel[9] % 2 == 0) ? 'K' : 'M';
+        }
+
+        public static bool checkControlNumber(string pesel)
+        {
+            int S = 0;
+            int M = 0;
+            int R = 0;
+            int multiplier = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                if((i+1) %4 == 1)
+                {
+                    multiplier = 1;
+                }
+                else if((i + 1) % 4 == 2)
+                {
+                    multiplier = 3;
+                }
+                else if ((i + 1) % 4 == 3)
+                {
+                    multiplier = 7;
+                }
+                else
+                {
+                    multiplier = 9;
+                }
+                S += int.Parse(pesel[i].ToString()) * multiplier;
+               
+            }
+            M = S % 10;
+            if (M == 0) 
+            { 
+                R = 0;
+            }
+            else
+            {
+                R = 10 - M;
+            }
+            if (R == int.Parse(pesel[10].ToString())) return true;
+            return false;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pesel");
+            string peselString = Console.ReadLine();
+            
+            if(peselString == "")
+            {
+                peselString = "55030101193";
+            }
+
+            if (checkGender(peselString) == 'M')
+            {
+                Console.WriteLine("Mężczyzna");
+            }
+            else {
+                Console.WriteLine("Kobieta");
+            }
+
+            if (checkControlNumber(peselString))
+            {
+                Console.WriteLine("Zgodność liczby kontrolnej");
+            }
+            else
+            {
+                Console.WriteLine("Niezgodność liczby kontrolnej");
+            }
+
+        }
+    }
+
+    
+
+}
